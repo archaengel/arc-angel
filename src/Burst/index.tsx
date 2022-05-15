035dbee89343331c76023e5276ef6f5ffe47239d
archaengel <>
2022-05-15 09:17:51 +0000

    Add mock save function

--- src/Burst/index.tsx
+++ src/Burst/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import Sketch from "react-p5";
+import React, { KeyboardEvent } from 'react'
+import Sketch from 'react-p5'
+import p5Types from 'p5'
 import {draw, setup } from './draw'
 
 
 export const Burst = () => {
+  const save = (p5: p5Types) => {
+    console.log(p5)
+  }
   return (
-    <Sketch setup={setup} draw={draw} />
+    <Sketch setup={setup} draw={draw} keyReleased={save} />
   )
 }
